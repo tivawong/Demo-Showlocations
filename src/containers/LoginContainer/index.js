e52a12354634ccf31e05283da94d7b4144a82f9f
tivaiice <tiva.wk@hotmail.com>
2020-05-28 14:16:19 +0700

    commit 5

--- src/containers/LoginContainer/index.js
+++ src/containers/LoginContainer/index.js
@@ -1,43 +1,52 @@
 
-import React from 'react';
-
-import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import LoginScreen from '../../screens/LoginScreen';
+import React from 'react'
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+import { StyleSheet, Text, View } from 'react-native'
+import LoginScreen from '../../screens/LoginScreen'
 
 class LoginContainer extends React.Component {
 	constructor(props) {
-		super(props);
-		this.state = {
-			language: "javascript",
-			isReady: false,
-		}
-	}
-	
-	_setState = (obj) => {
-		this.setState(obj)
+        super(props);
+        this.state = {
+          language: "script",
+          loadingFont: true,
+
+        }
+        this._loadingFont = this._loadingFont.bind(this);
+  	}
+  
+	componentDidMount () {
+		this._loadingFont()
 	}
 
-	async componentDidMount() {
+	async _loadingFont () {
 		await Font.loadAsync({
-			CSChatThai: require('../../fonts/CSChatThai.ttf'),
-			CSChatThaiUI: require('../../fonts/CSChatThaiUI.ttf'),
-		  ...Ionicons.font,
-		});
-		this.setState({ isReady: true });
-	}
+			CSChatThai: require("../../fonts/CSChatThai.ttf"),
+	        CSChatThaiUI: require("../../fonts/CSChatThaiUI.ttf")
+		})
 
-	render() {
-		if (!this.state.isReady) {
-			return <AppLoading />;
+		this.setState({ loadingFont: false })
+	}
+   
+    
+      _setState = () => {
+        this.setState({});
+      };
+
+      
+      render() {
+        const { loadingFont } = this.state
+
+		if (loadingFont) {
+		return <AppLoading />
 		}
 		return (
 			<LoginScreen
 				setComponentState={this._setState}
 				navigation={this.props.navigation}
 			/>
-		)
+		);
 	}
 }
 
